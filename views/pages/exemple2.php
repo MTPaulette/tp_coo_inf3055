a6dffdf05fc4c6993f4660b43ffe5343f7c2c3be
MTPaulette <mayoguepaulette@gmail.com>
2021-12-30 05:05:00 +0100

    initialisation du depot git pour le tp

---
+++ views/pages/exemple2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html  lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../styles/css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="../styles/css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="../styles/css/component.css" />
+	<link rel="stylesheet" type="text/css" href="../styles/css/footer.css" />
+	<script src="../js/modernizr.custom.js"></script>
+	<title>dashbord-adminPharmacie</title>
+</head>
+<body>
+<br /><br />
+	<p class="tt">Menu d�pliant � la google nexus</p><br /><br />
+		<div class="container">
+    		<?php include("../components/header.html"); ?>  
+
+			<header>
+				<h1>Menu du site du Google Nexus 7<span>Menu barre lat�rale comme sur le site <a href="http://www.google.com/nexus/index.html">Google Nexus 7</a> en haut � gauche :-)</span></h1>	
+			</header> 
+
+
+
+			<?php include("../components/footer.html"); ?> 			
+		</div><!-- /container -->
+		<script src="../js/classie.js"></script>
+		<script src="../js/gnmenu.js"></script>
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+</body>
+</html>
