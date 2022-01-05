e40b26ec5db6e58a82f9787c745f53e379c62e2c
Adams237 <83445201+Adams237@users.noreply.github.com>
2022-01-05 07:49:20 +0100

    class commande

---
+++ modele/Commande.class.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * 
+	 */
+	class Commande
+	{
+		$nomInternaute;
+		$listProduit  = array();
+		$livre
+		function __construct($nomInternaute)
+		{
+			$this->nomInternaute = $nomInternaute;
+			$this->livre = false;
+		}
+	}
+?>
