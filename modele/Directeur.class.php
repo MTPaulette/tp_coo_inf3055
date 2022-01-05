a945496c3fae9d73146cc9b2653fd6af8b3ca40c
Adams237 <83445201+Adams237@users.noreply.github.com>
2022-01-05 07:50:06 +0100

    class Directeur

---
+++ modele/Directeur.class.php
@@ -0,0 +1,30 @@
+<?php
+	/**
+	 * 
+	 */
+	include_once('Personne.class.php');
+	class ClassName extends AnotherClass
+	{
+		
+		function __construct($nom, $prenom, $tel, $adresse, $login, $password)
+		{
+			parent::__construct($nom, $prenom, $tel, $adresse, $login, $password);
+		}
+
+		public function addEmploye($employe){
+
+		}
+
+		public function deleteEmploye($employe){
+
+		}
+
+		public function suspendreEmploye($employe){
+
+		}
+
+		public function rechercherEmploye($employe){
+			
+		}
+	}
+?>
