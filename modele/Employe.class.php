8fff54340ad8382d7e4c6a1905336e788b0f2251
Adams237 <83445201+Adams237@users.noreply.github.com>
2022-01-05 07:50:39 +0100

    class Employe

---
+++ modele/Employe.class.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	 * 
+	 */
+
+	include_once('Personne.class.php');
+	class ClassName extends AnotherClass
+	{
+		
+		function __construct($nom, $prenom, $tel, $adresse, $login, $password)
+		{
+			parent::__construct($nom, $prenom, $tel, $adresse, $login, $password);
+		}
+
+		public function addProduit($produit){
+
+		}
+
+		public function deleteProduit($produit){
+
+		}
+
+		public function rechercher($produit){
+
+		}
+		public function vendre($produit){
+
+		}
+
+		public function consulterRelever(){
+			
+		}
+	}
+?>
