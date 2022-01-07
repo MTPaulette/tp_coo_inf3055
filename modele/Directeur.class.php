56d44a1d7f11b06c663a4abde4211c7c1ea0b7a0
Samuel-237 <83983263+Samuel-237@users.noreply.github.com>
2022-01-07 09:52:59 +0100

    Employe et Directeur
    Implémentation des méthodes des classes Directeur et Employe. La base de donnée a été modifiée selon les critères suivants:
    --> Ajout du champ 'image' dans la table Produit pour garder l'url des images dans la base de donnée
    --> Ajout d'une table 'nouveauproduit' pour garder la trace des produits nouvellement ajoutés n'ayant pas encore été marqués comme Vue

--- modele/Directeur.class.php
+++ modele/Directeur.class.php
@@ -2,29 +2,198 @@
 	/**
 	 * 
 	 */
+	require ('../Controleur/all_use_cases.php');
 	include_once('Personne.class.php');
-	class ClassName extends AnotherClass
+	class Directeur extends Personne
 	{
 		
 		function __construct($nom, $prenom, $tel, $adresse, $login, $password)
 		{
 			parent::__construct($nom, $prenom, $tel, $adresse, $login, $password);
 		}
-
-		public function addEmploye($employe){
-
+		public function Directeur($nom, $prenom, $tel, $adresse, $login, $password)
+		{
+			setNom($nom);
+			setPrenom($prenom);
+			setTelephone($tel);
+			setAdresse($adresse);
+			setLogin($login);
+			setMotDePasse($password);
 		}
 
-		public function deleteEmploye($employe){
-
-		}
-
-		public function suspendreEmploye($employe){
-
-		}
-
-		public function rechercherEmploye($employe){
+		public function addEmploye($nom, $prenom, $tel, $adresse, $etat, $login, $motDePasse){
 			
+			if (!empty(isset($nom))) {
+				if (!empty(isset($prenom))) {
+					if (!empty(isset($tel))) {
+						if (!empty(isset($adresse))) {
+							if (!empty(isset($etat))) {
+								if (!empty(isset($login))) {
+									if (!empty(isset($motDePasse))) {
+
+										
+										$baseDeDonnees = connecter();
+										$req = $baseDeDonnees->prepare('INSERT INTO employe(nom, prenom, tel, adresse, etat, login, motDePasse) VALUES(?, ?, ?, ?, ?, ?, ?)');
+										$req->execute(array($nom, $prenom, $tel, $adresse, $etat, $login, $motDePasse));
+										if ($req == true) {
+											?>
+											<script type="text/javascript">alert("Employé ajouté avec succès!");</script>
+											<?php
+
+										}
+										else{
+											?>
+											<script type="text/javascript">alert("Erreur lors de l'enregistrement de l'employé !");</script>
+											<?php
+										}
+
+
+									}
+									else{
+										?>
+											<script type="text/javascript">alert("Mot de passe non défini");</script>
+										<?php
+									}
+								}
+								else{
+									?>
+										<script type="text/javascript">alert("Login non défini!");</script>
+									<?php
+								}
+							}
+							else{
+							?>
+								<script type="text/javascript">alert("Etat non défini!");</script>
+							<?php	
+							}
+						}
+						else{
+							?>
+							<script type="text/javascript">alert("Adresse non défini !");</script>
+							<?php
+						}
+					}
+					else{
+						?>
+							<script type="text/javascript">alert("Numéro de téléphone non défini !");</script>
+						<?php
+					}
+				}
+				else{
+					?>
+						<script type="text/javascript">alert("Prénom non défini !");</script>
+					<?php
+				}
+			}
+			else{
+					?>
+						<script type="text/javascript">alert("Nom non défini !");</script>
+					<?php
+				}
+		}
+
+		public function deleteEmploye($login){
+			$baseDeDonnees = connecter();
+			//$req = $baseDeDonnees->prepare("DELETE FROM `employe` WHERE `employe`.`nom` =? ");
+			$req = $baseDeDonnees->prepare("UPDATE `employe` SET `etat` = 'Supprimé' WHERE `employe`.`login` =?");
+			$req->execute(array($login));
+			if ($req == true) {
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+		public function suspendreEmploye($login){
+			$baseDeDonnees = connecter();
+			$req = $baseDeDonnees->prepare("UPDATE `employe` SET `etat` = 'Suspendu' WHERE `employe`.`login` =?");
+			$req->execute(array($login));
+			if($req == true){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+		public function rechercherEmploye($nom){
+			
+		}
+
+		public function authentifier($login, $motDePasse)
+		{
+			if (!empty(isset($login) AND isset($motDePasse))) {
+				$baseDeDonnees = connecter();
+					$req = $baseDeDonnees->prepare('SELECT * FROM directeur WHERE login = ? AND motDePasse = ?');
+					$req->execute(array($login, $motDePasse));
+					if ($req == true) {
+						$data = $req->fetch();
+						session_start();
+						$_SESSION['idDirecteur'] = $data['id'];
+						$_SESSION['login'] = $login;
+						$_SESSION['motDePasse'] = $motDePasse;
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+			else{
+				?>
+					<script type="text/javascript">alert("Login ou mot de passe incorrect");</script>
+				<?php
+			}
+		}
+		public function deconnecter($url_connexion)
+		{
+			session_destroy();
+			header("Location: $url_connexion");
+		}
+		public function modifierInfos($directeur)
+		{
+			$nom = $directeur->nom;
+			$prenom = $directeur->prenom;
+			$tel = $directeur->tel;
+			$adresse = $directeur->adresse;
+			$login = $directeur->login;
+			$motDePasse = $directeur->motDePasse;
+			$baseDeDonnees = connecter();
+			$req = $baseDeDonnees->prepare("UPDATE `directeur` SET `nom` = ?, `prenom` = ?, `tel` = ?, `adresse` = ?, `login` = ?, `motDePasse` = ? WHERE `directeur`.`id` = ?");
+			$req->execute(array($nom, $prenom, $tel, $adresse, $login, $motDePasse));
+			if($req == true){
+				return true;
+			}
+			else{
+				return false;
+			}
+
+		}
+		public function consulterReleverVente()
+		{
+			$releve = array();
+			$baseDeDonnees = connecter();
+			$req1 = $baseDeDonnees->prepare("SELECT * FROM vente ");
+			$data = $req1->fetch();
+			while($data = $req1->fetch()){
+				$idProduit = $data['idProduit'];
+				$idEmploye = $data['idEmploye'];
+				$req2 = $baseDeDonnees->prepare("SELECT nom FROM produit WHERE id = ?");
+				$req2->execute(array($idProduit));
+				$req3 = $baseDeDonnees->prepare("SELECT nom FROM employe WHERE id = ?");
+				$req3->execute(array($idEmploye));
+				while($data2 = $req2->fetch() and $data3 = $req3->fetch()){
+					$nomProduit = $data2['nom'];
+					$nomEmploye = $data3['nom'];
+					$dateVente = $data['dateVente'];
+					$heure = $data['heure'];
+					$quantite = $data['quantite'];
+					$prix = $data['prix'];
+					$tab = array($nomProduit,$nomEmploye,$dateVente,$heure,$quantite,$prix);
+					$releve->append($tab);
+				}
+			}
+			return $releve;
 		}
 	}
 ?>
--- modele/Employe.class.php
+++ modele/Employe.class.php
@@ -2,33 +2,171 @@
 	/**
 	 * 
 	 */
-
+	require ('../Controleur/all_use_cases.php');
 	include_once('Personne.class.php');
 	class ClassName extends AnotherClass
 	{
-		
-		function __construct($nom, $prenom, $tel, $adresse, $login, $password)
+		private $etat;
+		function __construct($nom, $prenom, $tel, $adresse, $login, $password,$etat)
 		{
 			parent::__construct($nom, $prenom, $tel, $adresse, $login, $password);
+			$this->etat = $etat;
 		}
+
 
 		public function addProduit($produit){
-
+			$nom = $produit->nom;
+			$description = $produit->description;
+			$prix = $produit->prix;
+			$quantite = $produit->quantite;
+			$type = $produit->type;
+			$baseDeDonnees = connecter();
+			$req = $baseDeDonnees->prepare("INSERT INTO produit(nom, description, prix, quantite, type) VALUES(?, ?, ?, ?, ?)");
+			$req->execute(array($nom, $description, $prix, $quantite, $type));
+			$req2 = $baseDeDonnees->prepare("INSERT INTO nouveauproduit(nom, description, prix, quantite, type) VALUES(?, ?, ?, ?, ?)");
+			$req2->execute(array($nom, $description, $prix, $quantite, $type));
+			if($req == true and $req2 ==true ){
+				return true;
+			}
+			else{
+				return false;
+			}
 		}
 
-		public function deleteProduit($produit){
-
+		public function deleteProduit($nom){
+			$baseDeDonnees = connecter();
+			$req = $baseDeDonnees->prepare("DELETE FROM `produit` WHERE `produit`.`nom` =? ");
+			$req->execute(array($nom));
+			if($req == true){
+				return true;
+			}
+			else{
+				return false;
+			}
 		}
 
 		public function rechercher($produit){
 
 		}
-		public function vendre($produit){
-
+		public function vendre($produit,$employe,$quantite){
+			$dateVente = date("l F d, Y"); //Récupérer la date courante
+			$heure = date("H:i:s");
+			$nomProduit = $produit->nom;
+			$idEmploye = $employe->id;
+			$baseDeDonnees = connecter();
+			$req1 = $baseDeDonnees->prepare("SELECT quantite, id FROM produit WHERE nom = ?");
+			$req1->execute(array($nomProduit));
+			$data = $req1->fetch(); //Récupération de la sélection dans la variable data
+			$nouvelle_qte = $data['quantite']-$quantite; //Décrémentation de la quantité
+			$idProduit = $data['id'];
+			$req2 = $baseDeDonnees->prepare("UPDATE `produit` SET `quantite` = ? WHERE `produit`.`nom` =?");
+			$req2->execute(array($nouvelle_qte,$nomProduit));
+			$req = $baseDeDonnees->prepare("INSERT INTO vendre(idProduit, idEmploye, dateVente, heure, quantite) VALUES (?,?,?,?,?)");
+			$req->execute(array($idProduit, $idEmploye, $dateVente, $heure, $quantite));
+			if($req == true and $req1 == true and $req2 == true){
+				return true;
+			}
+			else{
+				return false;
+			}
 		}
 
-		public function consulterRelever(){
-			
+		public function consulterReleverVente(){
+			$releveVente = array();
+			$baseDeDonnees = connecter();
+			$req1 = $baseDeDonnees->prepare("SELECT * FROM vente ");
+			$data = $req1->fetch();
+			while($data = $req1->fetch()){
+				$idProduit = $data['idProduit'];
+				$idEmploye = $data['idEmploye'];
+				$req2 = $baseDeDonnees->prepare("SELECT nom FROM produit WHERE id = ?");
+				$req2->execute(array($idProduit));
+				$req3 = $baseDeDonnees->prepare("SELECT nom FROM employe WHERE id = ?");
+				$req3->execute(array($idEmploye));
+				while($data2 = $req2->fetch() and $data3 = $req3->fetch()){
+					$nomProduit = $data2['nom'];
+					$nomEmploye = $data3['nom'];
+					$dateVente = $data['dateVente'];
+					$heure = $data['heure'];
+					$quantite = $data['quantite'];
+					$prix = $data['prix'];
+					$tab = array($nomProduit,$nomEmploye,$dateVente,$heure,$quantite,$prix);
+					$releveVente->append($tab);
+				}
+			}
+			return $releveVente;
+		}
+		/*
+		--> Cette fonction retourne un tableau dont les éléments sont des tableaux de 6 éléments.
+		--> Le premier élément correspond au nom du produit, le deuxième au nom de l'employé, etc...
+		*/
+		public function consulterReleverAjout(){
+			$releveAjout = array();
+			$baseDeDonnees = connecter();
+			$req1 = $baseDeDonnees->prepare("SELECT * FROM nouveauproduit ");
+			$data = $req1->fetch();
+			while($data = $req1->fetch()){
+				$idEmploye = $data['idEmploye'];
+				$req3 = $baseDeDonnees->prepare("SELECT nom FROM employe WHERE id = ?");
+				$req3->execute(array($idEmploye));
+				while($data3 = $req3->fetch()){
+					$nomProduit = $data['nom'];
+					$nomEmploye = $data3['nom'];
+					$dateVente = $data['dateVente'];
+					$heure = $data['heure'];
+					$quantite = $data['quantite'];
+					$prix = $data['prix'];
+					$tab = array($nomProduit,$nomEmploye,$dateVente,$heure,$quantite,$prix);
+					$releveAjout->append($tab);
+				}
+			}
+			return $releveAjout;
+		}
+		public function authentifier($login, $motDePasse){
+			if (!empty(isset($login) AND isset($motDePasse))) {
+				$baseDeDonnees = connecter();
+					$req = $baseDeDonnees->prepare('SELECT * FROM employe WHERE login = ? AND motDePasse = ?');
+					$req->execute(array($login, $motDePasse));
+					if ($req == true) {
+						$data = $req->fetch();
+						session_start();
+						$_SESSION['idEmploye'] = $data['id'];
+						$_SESSION['login'] = $login;
+						$_SESSION['motDePasse'] = $motDePasse;
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+			else{
+				?>
+					<script type="text/javascript">alert("Login ou mot de passe incorrect");</script>
+				<?php
+			}
+		}
+		public function deconnecter(){
+			session_destroy();
+			header("Location: $url_connexion");
+		}
+		public function modifierInfos($employe)
+		{
+			$nom = $employe->nom;
+			$prenom = $employe->prenom;
+			$tel = $employe->tel;
+			$adresse = $employe->adresse;
+			$login = $employe->login;
+			$motDePasse = $employe->motDePasse;
+			$baseDeDonnees = connecter();
+			$req = $baseDeDonnees->prepare("UPDATE `employe` SET `nom` = ?, `prenom` = ?, `tel` = ?, `adresse` = ?, `login` = ?, `motDePasse` = ? WHERE `employe`.`id` = ?");
+			$req->execute(array($nom, $prenom, $tel, $adresse, $login, $motDePasse));
+			if($req == true){
+				return true;
+			}
+			else{
+				return false;
+			}
+
 		}
 	}
 ?>
