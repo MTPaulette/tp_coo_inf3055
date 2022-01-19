685f7570f20fd4cbbd9e37860e6201ca41e7a27a
MTPaulette <mayoguepaulette@gmail.com>
2022-01-19 13:59:59 +0100

    debut de dynamisation du code avec jquery

--- controllers/ajax/employe/vendreProduit.js
+++ controllers/ajax/employe/vendreProduit.js
@@ -4,14 +4,20 @@ $(function () {
     $('#nom').click(function(e){
         $.ajax({
             url: "../../../controllers/php/employe/chargerProduit.php",
-            type: "GET",
-            
+            type: "GET",          
             contentType: "application/json",
             dataType: "JSON",
             
             success: function(produits) {
+                //$(produits) = produits;
                 console.log(produits);
-                console.log(JSON.parse(produits));
+                $.each(produits, function(i,produit) {
+                    $html += "<option value="+produit[i]+">"+produit[2]+"</option><br>";
+                    $html += "<option value="+produit['nomp']+">"+produit['nomp']+"</option><br>";
+                })
+				
+                console.log(html);
+                $('#nom').prepend(html)	
             },
             
         })
